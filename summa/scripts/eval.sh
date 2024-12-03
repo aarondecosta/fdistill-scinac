7cd8fa21ce47fdc9c2a05914cfbe601ae63e86bd
Aaron DeCosta <aarondom@beluga4.int.ets1.calculquebec.ca>
2024-12-03 00:54:30 -0500

    updated eval script

--- summa/scripts/eval.sh
+++ summa/scripts/eval.sh
@@ -1,12 +1,25 @@
-export MODEL_NAME=''
+#!/bin/sh
+
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=16000
+#SBATCH --time=0-01:00
+#SBATCH --account=def-lilimou
+#SBATCH --gres=gpu:1
+
+nvidia-smi
+
+export MODEL_NAME='sshleifer/distilbart-xsum-6-6'
 echo $MODEL_NAME
 export BEAM=6
-python3 ../run_eval.py \
+export SAVE_PATH=runs/pred-$(date +%m-%d-%y--%T)-teacher-eval
+
+
+python3 run_eval.py \
   --model_name $MODEL_NAME \
-  --input_path '' \
-  --save_path '' \
-  --reference_path '' \
-  --score_path '' \
+  --input_path xsum/test.source \
+  --save_path $SAVE_PATH/res.out \
+  --reference_path xsum/test.target \
+  --score_path $SAVE_PATH/metrics.json \
   --num_beams $BEAM\
   --length_penalty 0.5\
   --device cuda
